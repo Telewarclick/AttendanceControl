251f81724c61e6baeb1319b9758d8318cdc7ffc6
Telewarclick <kiba9904@gmail.com>
2020-06-14 00:04:09 +0300

    create project

---
+++ AttendanceControl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AttendanceControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
