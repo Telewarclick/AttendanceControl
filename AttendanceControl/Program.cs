97b8c5ea378bf49088925c6ac2b77256a9c38cae
Telewarclick <kiba9904@gmail.com>
2020-06-14 00:34:16 +0300

    lab13 chain of responsibility
    lab13 chain of responsibility

--- AttendanceControl/Program.cs
+++ AttendanceControl/Program.cs
@@ -31,6 +31,22 @@ namespace DesignPatterns
             o.State = "Vasylenko Olexandr";
             o.SetMemento(c.Memento);
 
+
+            // chain responsibility pattern 13
+            var task1 = new Job("Dry oil around engine", new List<string> { "Rag", "Gas", "Mop" });
+            var task2 = new Job("Dry flor around circulating pump", new List<string> { "Rag", "Mop" });
+            var task3 = new Job("Change bearing", new List<string> { "Bearing", "Oil" });
+            var task4 = new Job("Brew a gap in circulating pump", new List<string> { "Welding machine" });
+            var task5 = new Job("Repair engine", new List<string> { "Key", "Bearing", "Oil" });
+            var locksmith = new Locksmith(null);
+            var welder = new Welder(locksmith);
+            var housemaid = new Housemaid(welder);
+            housemaid.HandleJob(task1);
+            housemaid.HandleJob(task2);
+            housemaid.HandleJob(task3);
+            housemaid.HandleJob(task4);
+            housemaid.HandleJob(task5);
+
             Console.Read();
         }
     }
