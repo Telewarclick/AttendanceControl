a27333d9baf612db13c9a636f6ad079703f44b7e
Illya <kiba9904@gmail.com>
2020-06-17 03:35:58 +0300

    Add DTO to BLL

---
+++ BLL/DTO/ActualTimeDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BLL.DTO
+{
+    public class ActualTimeDTO
+    {
+        public int Id { get; set; }
+        public DateTime TimeEnter { get; set; }
+        public DateTime TimeExit { get; set; }
+        public int WorkDayId { get; set; }
+        public WorkDayDTO WorkDay { get; set; }
+    }
+}
---
+++ BLL/DTO/WorkDayDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace BLL.DTO
+{
+    public class WorkDayDTO
+    {
+        
+    }
+}
